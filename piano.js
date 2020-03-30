5a57c102f9996afc0dfca83eaafd3738672f7fb4
David Kim <jk2537@cornell.edu>
2020-03-30 04:16:59 -0500

    Overhauled & updated to match website design

--- piano.js
+++ piano.js
@@ -1,11 +1,34 @@
 import parse from "./music/parser.js";
 
+var notes;
+var tempo;
+var div;
+
+var music;
+
+// Default CSV/MIDI file.
+var csvFile = "/music/csv/psalm_139.csv";
+var midiFile = "/music/midi/psalm_139.mid";
+
+// Selects a random song from the music directory.
+const selectRandomSong = function () {
+  let songList = ["psalm_139", "holy_is_the_lord", "fur_elise"];
+  let csvDir = "/music/csv/";
+  let musicDir = "/music/midi/";
+
+  let chosenSong = songList[Math.floor(Math.random() * songList.length)];
+  csvFile = csvDir + chosenSong + ".csv";
+  midiFile = musicDir + chosenSong + ".mid";
+  // console.log(csvFile);
+}
+
 const loadMIDI = async function () {
-  const music = await parse("/music/csv/psalm_139.csv");
+  selectRandomSong();
+  music = await parse(csvFile);
   // console.log(music);
-  const notes = music.filter(
+  notes = music.filter(
     d => d.type === "Note_on_c" || d.type === "Note_off_c");
-  console.log(notes);
+  // console.log(notes);
 
   let md = createPianoChart(notes);
 
@@ -74,26 +97,117 @@ const loadMIDI = async function () {
   }
 
   // Tempo needed to calculate the speed of the animation
-  let tempo = music.filter(d => d.type === "Tempo");
+  tempo = music.filter(d => d.type === "Tempo");
   tempo = tempo[0].channel;
 
-  let div = music.filter(d => d.type === "Header");
+  div = music.filter(d => d.type === "Header");
   div = div[0].velocity;
 
-  // Now, process the MIDI file to show the animations.
-  d3.selectAll("#buttonContainer").append("button")
-    .text("Click me!")
-    .attr("type", "button")
+  asyncSetup();
+}
+
+// Wrapper to call for dependencies and then show button to be ready.
+const asyncSetup = async function () {
+  await fetchDependencies();
+}
+
+// Fetches resources needed to load the music file concurrently with the 
+// visual animations.
+const fetchDependencies = async function () {
+  MIDIjs.message_callback = function (msg) {
+    let promise = new Promise((resolve) => {
+      if (msg.substring(0, 7) === "Playing") {
+        // console.log(msg);
+        resolve("Ready!");
+      }
+    })
+    promise.then(function () {
+      setTimeout(MIDIjs.pause, 15); // to ensure music STOPS after init loading
+      setButtonReady();
+    });
+  }
+  MIDIjs.play(midiFile);
+}
+
+// Allows the button to be pressed, and changes its status to "ready".
+const setButtonReady = function () {
+  let button = d3.select("g#button")
     .on("click", function () {
       play(notes, tempo, div);
     });
+  button.select("text#buttonText")
+    .transition().duration(100)
+    .text("Ready!");
+  // console.log("Button ready!");
+}
+
+// Starts the MIDI file.
+const play = function (notes, tempo, div) {
+  MIDIjs.resume();
+  // console.log("Start!");
+
+  setTimeout(() => {
+    notes.forEach(row => {
+      setTimeout(() => {
+        if (row.type == "Note_off_c") {
+          noteOff(row.note);
+        } else if (row.type == "Note_on_c") {
+          if (row.velocity == 0) {
+            noteOff(row.note);
+          } else {
+            noteOn(row.note);
+          }
+        } else {
+          console.log("Done!");
+        }
+      }, row.time * (500 / div) * (tempo / 500000));
+      // Long story short, some calculations were done based on the MIDICSV 
+      // documentation's info about "Tempo", "Header", and the random_chords file.
+    });
+  }, Math.floor((music.length) / 16)); // <-- This value here should be adjusted 
+  //         with more precision to help with the delay of the visual animation.
+
+  // Disables button after starting the animation
+  let button = d3.select("g#button")
+    .on("click", null);
+  button.select("text#buttonText")
+    .transition().duration(100)
+    .text("Enjoy! <3");
+}
+
+// Changes the color of a note when pressed (played).
+const noteOn = function (note) {
+  let n = d3.select("#note" + note);
+  if (n.attr("class") == "white") {
+    n.selectAll("rect")
+      .transition().duration(50)
+      .attr("fill", "#ff9999");
+  } else {
+    n.selectAll("rect")
+      .transition().duration(50)
+      .attr("fill", "#ff6666");
+  }
+}
+
+// Reverts the color of a note back when depressed.
+const noteOff = function (note) {
+  let n = d3.select("#note" + note);
+  if (n.attr("class") == "white") {
+    n.selectAll("rect")
+      .transition().duration(100)
+      .attr("fill", "white");
+  } else {
+    n.selectAll("rect")
+      .transition().duration(100)
+      .attr("fill", "black");
+  }
 }
 
 const createPianoChart = function (notes) {
   // Chart specs
-  let svg = d3.select("p#piano").append("svg")
-    .attr("width", "1500")
-    .attr("height", "600");
+  let svg = d3.select("#piano").append("svg")
+    .attr("width", "1400")
+    .attr("height", "500");
   let w = svg.attr("width");
   let h = svg.attr("height");
   let margins = { "top": 50, "right": 50, "bottom": 50, "left": 50 };
@@ -116,17 +230,18 @@ const createPianoChart = function (notes) {
 
   let blackKeyHeight = ch / 4 * 1.2; // Adjust this to fit chart properly
 
-  // Here for now to help with debugging the area
+  // Contains the text elements, buttons, etc.
   let canvas = svg.append("g").attr("id", "canvas")
     .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
-  canvas.append("rect")
-    .attr("x", "0")
-    .attr("y", "0")
-    .attr("width", cw)
-    .attr("height", ch)
-    .attr("stroke", "none")
-    .attr("stroke-width", "0.5")
-    .attr("fill", "none");
+  // Rectangle below is for debugging, but can be added.
+  // canvas.append("rect")
+  //   .attr("x", "0")
+  //   .attr("y", "0")
+  //   .attr("width", cw)
+  //   .attr("height", ch)
+  //   .attr("stroke", "grey")
+  //   .attr("stroke-width", "0.5")
+  //   .attr("fill", "none");
 
   // Actual minmax values of the MIDI file's notes
   const min = d3.min(notes, (d) => { return d.note });
@@ -188,6 +303,43 @@ const createPianoChart = function (notes) {
       .attr("y2", ch / 2);
   }
 
+  // Container to hold all the chart elements together.
+  canvas.append("text") // Title text
+    .text("MIDI Visualizer")
+    .attr("x", cw / 2)
+    .attr("y", ch / 10)
+    .attr("font-size", "30")
+    .attr("letter-spacing", "1.6px")
+    .style("text-anchor", "middle")
+    .style("font-weight", "bold");
+  canvas.append("text") // Header text
+    .text("Refresh the page to get a random song! Currently working on custom MIDI...")
+    .attr("x", cw / 2)
+    .attr("y", ch / 5)
+    .attr("font-size", "22")
+    .attr("letter-spacing", "1.6px")
+    .style("text-anchor", "middle");
+  let button = canvas.append("g") // Button grouped into a single element
+    .attr("id", "button");
+  button.append("rect") // Button
+    .attr("x", cw / 2 - 50)
+    .attr("y", 5 * ch / 6)
+    .attr("width", "100")
+    .attr("height", "50")
+    .attr("stroke", "#4C8F50")
+    .attr("stroke-width", "0.5")
+    .attr("fill", "#4CC550")
+    .attr("id", "buttonRect");
+  button.append("text") // Button text
+    .text("Loading...")
+    .attr("x", cw / 2)
+    .attr("y", 5 * ch / 6 + 30)
+    .attr("font-size", "14")
+    .attr("letter-spacing", "1px")
+    .attr("id", "buttonText")
+    .attr("fill", "white")
+    .style("text-anchor", "middle");
+
   return {
     svg: svg,
     margins: margins,
@@ -199,56 +351,30 @@ const createPianoChart = function (notes) {
   };
 }
 
-// Starts the MIDI file.
-const play = function (notes, tempo, div) {
-  MIDIjs.play("music/midi/psalm_139.mid");
-  setTimeout(() => {
-    notes.forEach(row => {
-      setTimeout(() => {
-        if (row.type == "Note_off_c") {
-          noteOff(row.note);
-        } else if (row.type == "Note_on_c") {
-          if (row.velocity == 0) {
-            noteOff(row.note);
-          } else {
-            noteOn(row.note);
-          }
-        } else {
-          console.log("Done!");
-        }
-      }, row.time * (500 / div) * (tempo / 500000));
-      // Long story short, some calculations were done based on the MIDICSV 
-      // documentation's info about "Tempo", "Header", and the random_chords file.
-    });
-  }, 500);
-}
-
-// Changes the color of a note when pressed (played).
-const noteOn = function (note) {
-  let n = d3.select("#note" + note);
-  if (n.attr("class") == "white") {
-    n.selectAll("rect")
-      .transition().duration(50)
-      .attr("fill", "#ff9999");
-  } else {
-    n.selectAll("rect")
-      .transition().duration(50)
-      .attr("fill", "#ff6666");
-  }
-}
-
-// Reverts the color of a note back when depressed.
-const noteOff = function (note) {
-  let n = d3.select("#note" + note);
-  if (n.attr("class") == "white") {
-    n.selectAll("rect")
-      .transition().duration(100)
-      .attr("fill", "white");
-  } else {
-    n.selectAll("rect")
-      .transition().duration(100)
-      .attr("fill", "black");
-  }
+// Makes XMLHttpRequests (XHR) to a resource at a given URL.
+// Turns out I didn't need to do this. Wasted 12 hours fml.
+const makeRequest = function (method, url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
 }
 
 loadMIDI();
